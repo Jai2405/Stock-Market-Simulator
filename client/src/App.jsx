214a1e82377d396276e027bd699597041a75e40f
Jai2405 <64653358+Jai2405@users.noreply.github.com>
2025-01-06 20:04:48 +0530

    Update App.jsx

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,5 +1,5 @@
 import { useEffect, useState } from 'react'
-import {BrowserRouter as Router, Route, Routes} from "react-router-dom";
+import {BrowserRouter as Router, Route, Routes, Navigate} from "react-router-dom";
 import MainLayout from './Components/Layout/MainLayout';
 import Home from './pages/Home';
 import Portfolio from './pages/Portfolio'
@@ -9,19 +9,22 @@ import Register from './pages/Register';
 import Login from './pages/Login';
 import './App.css'
 
-
 export default function App() {
   return (
-      <Router>
-        <MainLayout>
-          <Routes>
-            <Route path="/" element={<Home />} />
-            <Route path="/register" element={<Register />} />
-            <Route path="/login" element={<Login />} />
-            <Route path="/portfolio" element={<Portfolio />} />
-            <Route path="/stock/:symbol" element={<Stock />} />
-          </Routes>
-        </MainLayout>
-      </Router>
+    <Router>
+      <Routes>
+        {/* Direct access to "/" redirects to "/login" */}
+        <Route path="/" element={<Navigate to="/login" replace />} />
+        <Route path="/register" element={<Register />} />
+        <Route path="/login" element={<Login />} />
+      </Routes>
+      <MainLayout>
+        <Routes>
+          <Route path="/home" element={<Home />} />
+          <Route path="/portfolio" element={<Portfolio />} />
+          <Route path="/stock/:symbol" element={<Stock />} />
+        </Routes>
+      </MainLayout>
+    </Router>
   );
 }
